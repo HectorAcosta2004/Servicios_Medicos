38056ab0ed76107533264caf2b1d3fe59371c4a0
HectorAcosta2004 <1220072@unav.edu.mx>
2025-05-01 19:13:15 -0700

    detalles del menu

--- Servicios_Medicos/pages/dashboard_admin.php
+++ Servicios_Medicos/pages/dashboard_admin.php
@@ -58,7 +58,7 @@
           </a>
         </li>
         <li class="nav-item">
-          <a class="nav-link " href="../pages/cer_citas.php">
+          <a class="nav-link " href="../pages/ver_citas.php">
             <div
               class="icon icon-shape icon-sm border-radius-md text-center me-2 d-flex align-items-center justify-content-center">
               <i class="ni ni-credit-card text-dark text-sm opacity-10"></i>
